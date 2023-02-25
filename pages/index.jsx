ab09d352a00ab0699acfe8d1da50a33b0cb6009f
Medea <logmedha09@gmail.com>
2023-02-25 17:28:21 +0000

    changed about me again

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,8 @@ export default function HomePage() {
     <Layout pageTitle="Home">
       <div id="about">
         <h2>About me</h2>
-        <p>I'm a young full-stack developer from the UK.</p>
-        <p>I like creating <strong>full stack websites</strong> with <strong>MongoDB</strong> in <strong>Flask</strong> or <strong>Next.js</strong>.</p>
+        <p>I'm a young <strong>full-stack developer</strong> from the UK who codes for <strong>fun</strong>.</p>
+        <p>I like creating websites with <strong>MongoDB</strong> in <strong>Flask</strong> or <strong>Next.js</strong>.</p>
         <p>I also enjoy playing and creating <strong>music</strong>.</p>
       </div>
     </Layout>

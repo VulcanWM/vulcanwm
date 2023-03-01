f73e45ee8a6ac41c6a7230a4c7d25911e96783fc
Medea <logmedha09@gmail.com>
2023-03-01 20:56:59 +0000

    added the projects dictionary in just one file

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,75 +1,12 @@
 import Layout from '../components/layout';
 import styles from '../styles/projects.module.css';
 import ProjectCard from '../components/projectcard'
-
-const projects = {
-  "Distribute Time": {
-    "Link": "https://distribute-time.vulcanwm.repl.co",
-    "Desc": "automatically make your own timetable",
-    "Tags": ["Flask", "HTML,CSS,JS", "Tool"]
-  }, 
-  "What If": {
-    "Link": "https://what-if.vulcanwm.repl.co",
-    "Desc": "a website where you will face hypothetical scenarios",
-    "Tags": ["Flask", "HTML,CSS,JS", "MongoDB", "Forum", "ReplAuth"]
-  },
-  "OvalEyes": {
-    "Link": "https://ovaleyes.ovaleyes.repl.co",
-    "Desc": "a social media website",
-    "Tags": ["Flask", "HTML,CSS,JS", "MongoDB", "Forum"]
-  }, 
-  "We Greek": {
-    "Link": "https://wegreek.vulcanwm.repl.co",
-    "Desc": "a greek mythology wikipedia",
-    "Tags": ['Flask', 'HTML,CSS,JS', 'MongoDB', "Forum"]
-  }, 
-  "Escape the Punch": {
-    "Link": "https://escape-the-punch.vulcanwm.repl.co",
-    "Desc": "try to escape the ghost's punch",
-    "Tags": ["kaboom.js", 'Game']
-  },
-  "wizard vs. snake": {
-    "Link": "https://wizard-vs-snake.vulcanwm.repl.co/",
-    "Desc": "escape the snakes while exploring wizard world",
-    "Tags": ['kaboom.js', 'express.js', 'HTML,CSS,JS', 'ReplitDB', 'ReplAuth']
-  }, 
-  // "Munity": {
-  //   "Link": "https://munity.vulcanwm.repl.co",
-  //   "Desc": "test yourself on song lyrics",
-  //   "Status": "progress",
-  //   "Tags": ["Flask", "HTML,CSS,JS", "MongoDB", "Game"]
-  // }, 
-  "Jasonism": {
-    "Link": "https://jasonism.vulcanwm.repl.co",
-    "Desc": "make your block of wood mighty",
-    "Tags": ['Flask', 'HTML,CSS,JS', 'MongoDB', "Game"]
-  }, 
-  // "FRANK": {
-  //   "Link": "https://frank.vulcanwm.repl.co",
-  //   "Desc": "a multi-purpose website",
-  //   "Status": "progress",
-  //    "Tags": ['Flask', 'HTML,CSS,JS', 'MongoDB', "Forum"]
-  // }, 
-  "Drinks Cabin": {
-    "Link": "https://drinks-cabin.vulcanwm.repl.co",
-    "Desc": "own your own drinks company",
-     "Tags": ['Flask', 'HTML,CSS,JS', 'MongoDB', "Game"]
-  }, 
-  "BesucheResort": {
-    "Link": "https://besucheresort.vulcanwm.repl.co",
-    "Desc": "search for money in a resort",
-     "Tags": ['Flask', 'HTML,CSS,JS', 'MongoDB', "AFrame", "Game"]
-  },
-  "Jason Clicker": {
-    "Link": "https://jason-clicker.vulcanwm.repl.co",
-    "Desc": "try to make jason stronger by clicking",
-    "Tags": ["kaboom.js", "Game"]
-  }
-}
+import {projects} from '../lists/projects'
 
 export default function ProjectsPage() {
   return (
     <Layout pageTitle="Projects">
+      <h2>My Skills</h2>
       <div className={styles.projects}>
         { 
           Object.keys(projects).map((project, index) => ( 

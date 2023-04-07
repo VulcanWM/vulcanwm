3582bf19b4af42bae9bbdb7caa1a7c6ed409be84
Medea <vulcanwmemail@gmail.com>
2023-04-07 09:10:50 +0100

    Updating strong tag in index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
         <h2>About me</h2>
         <p>I'm a young <strong>full-stack developer</strong> from the UK who codes for <strong>fun</strong>.</p>
         <p>I like creating websites with <strong>MongoDB</strong> in <strong>Flask</strong> or <strong>Next.js</strong>.</p>
-        <p>I also like developing apps with <strong>React Native and Expo</strong>.</p>
+        <p>I also like developing apps with <strong>React Native</strong> and <strong>Expo</strong>.</p>
         <p>I'm a <strong>trusted member</strong> and <strong>tag moderator</strong> at <a target="_blank" href="https://dev.to">dev.to</a>.</p>
         <p>I enjoy playing and creating <strong>music</strong>.</p>
       </div>

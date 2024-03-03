21850557671b1bcea6c7e38a4d4d9f91e5c1f34a
VulcanWM <logmedha09@gmail.com>
2024-03-03 13:22:37 +0000

    changed age

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <Layout pageTitle="Home">
       <div id="about">
         <h2>About me</h2>
-        <p>I'm a young <strong>full-stack developer</strong> from the UK who codes for <strong>fun</strong>.</p>
+        <p>I'm a 15 yo <strong>full-stack developer</strong> from the UK who codes for <strong>fun</strong>.</p>
         <p>I like creating websites with <strong>MongoDB</strong> in <strong>Flask</strong> or <strong>Next.js</strong>.</p>
         <p>I also like developing apps with <strong>React Native</strong> and <strong>Expo</strong>.</p>
         <p>I'm a <strong>trusted member</strong> and <strong>tag moderator</strong> at <a target="_blank" href="https://dev.to">dev.to</a>.</p>
